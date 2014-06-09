8eab501094813ad693ef16b7be7cde8ee6765ff2
Karl Hobley <karl@torchbox.com>
2014-06-09 10:45:33 +0100

    Fixed typo

--- wagtail/wagtailcore/views.py
+++ wagtail/wagtailcore/views.py
@@ -3,8 +3,6 @@ from django.http import Http404
 
 def get_pages_for_path(page, path_components):
     while page is not None:
-        path_components = path_components[1:]
-
         # Yield this page
         yield page.specific, path_components
 
@@ -14,6 +12,9 @@ def get_pages_for_path(page, path_components):
 
         # Get next page
         page = page.get_children().filter(slug=path_components[0]).first()
+
+        # Remove first path component
+        path_components = path_components[1:]
 
 
 def serve(request, path):
@@ -25,6 +26,7 @@ def serve(request, path):
     path_components = [component for component in path.split('/') if component]
 
     for page, remaining_components in get_pages_for_path(request.site.root_page, path_components):
+        print page, remaining_components
         response = page.process_request(request, remaining_components)
 
         if response is not None:
