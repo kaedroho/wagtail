bfbde4d1e27ba5c120c9c54966f847cbf5c45765
Karl Hobley <karlhobley10@gmail.com>
2015-05-24 18:45:10 +0100

    Install wheel before making wheel files

--- scripts/drone/deploy.sh
+++ scripts/drone/deploy.sh
@@ -2,9 +2,22 @@
 
 pushd /var/cache/drone/src/github.com/kaedroho/wagtail
 
-npm install
-python3 setup.py sdist
-python3 setup.py bdist_wheel
-python2 setup.py bdist_wheel
+echo "Building packages..."
+
+# Install NPM dependencies for building SASS
+npm install > /dev/null
+
+# Create sdist
+python3 setup.py sdist > /dev/null
+
+# Create wheel for Python 3
+pip3 install wheel
+python3 setup.py bdist_wheel > /dev/null
+
+# Create wheel for Python 2
+pip2 install wheel
+python2 setup.py bdist_wheel > /dev/null
+
+# TODO Upload!
 
 popd
