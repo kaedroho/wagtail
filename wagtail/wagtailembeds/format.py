8e610365b6d96b95a0ebd098c7d9e596e5639322
Karl Hobley <karlhobley10@gmail.com>
2015-10-28 16:41:00 +0000

    Moved ratio calculation to Embed model

--- wagtail/wagtailembeds/format.py
+++ wagtail/wagtailembeds/format.py
@@ -1,5 +1,3 @@
-from __future__ import division  # Use true division
-
 from django.template.loader import render_to_string
 
 from wagtail.wagtailembeds import embeds
@@ -9,16 +7,10 @@ def embed_to_frontend_html(url):
     try:
         embed = embeds.get_embed(url)
 
-        # Work out ratio
-        if embed.width and embed.height:
-            ratio = str(embed.height / embed.width * 100) + "%"
-        else:
-            ratio = "0"
-
         # Render template
         return render_to_string('wagtailembeds/embed_frontend.html', {
             'embed': embed,
-            'ratio': ratio,
+            'ratio': embed.ratio_css or '0',
         })
     except embeds.EmbedException:
         return ''
--- wagtail/wagtailembeds/models.py
+++ wagtail/wagtailembeds/models.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+from __future__ import division  # Use true division
 
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
@@ -40,6 +41,17 @@ class Embed(models.Model):
     class Meta:
         unique_together = ('url', 'max_width')
         verbose_name = _('Embed')
+
+    @property
+    def ratio(self):
+        if embed.width and embed.height:
+            return = embed.height / embed.width
+
+    @property
+    def ratio_css(self):
+        ratio = self.ratio
+        if ratio:
+            return str(ratio * 100) + "%"
 
     def __str__(self):
         return self.url
