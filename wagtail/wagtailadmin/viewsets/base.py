7633b99e7eb274d8c5b6363344b411e7229beca4
Karl Hobley <karlhobley10@gmail.com>
2015-12-23 15:24:27 +0000

    Added view sets

---
+++ wagtail/wagtailadmin/viewsets/base.py
@@ -0,0 +1,9 @@
+class ViewSet(object):
+    def __init__(self, name, **kwargs):
+        self.name = name
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def get_urlpatterns(self):
+        return []
