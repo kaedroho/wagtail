b8b10df2a311c1820c4a341aa19a52a9e39f5aa9
Karl Hobley <karlhobley10@gmail.com>
2015-12-23 15:24:43 +0000

    Converted wagtailsites to use viewsets

--- wagtail/wagtailsites/views.py
+++ wagtail/wagtailsites/views.py
@@ -2,56 +2,47 @@ from __future__ import absolute_import, unicode_literals
 
 from django.utils.translation import ugettext_lazy as __
 
-from wagtail.wagtailadmin.views.generic import CreateView, DeleteView, EditView, IndexView
 from wagtail.wagtailcore.models import Site
 from wagtail.wagtailcore.permissions import site_permission_policy
 from wagtail.wagtailsites.forms import SiteForm
+from wagtail.wagtailadmin.views import generic
+from wagtail.wagtailadmin.viewsets.model import ModelViewSet
 
 
-class Index(IndexView):
-    permission_policy = site_permission_policy
-    model = Site
-    context_object_name = 'sites'
+class IndexView(generic.IndexView):
     template_name = 'wagtailsites/index.html'
-    add_url_name = 'wagtailsites:add'
     page_title = __("Sites")
     add_item_label = __("Add a site")
-    header_icon = 'site'
 
 
-class Create(CreateView):
-    permission_policy = site_permission_policy
-    form_class = SiteForm
+class CreateView(generic.CreateView):
     page_title = __("Add site")
     success_message = __("Site '{0}' created.")
-    add_url_name = 'wagtailsites:add'
-    edit_url_name = 'wagtailsites:edit'
-    index_url_name = 'wagtailsites:index'
     template_name = 'wagtailsites/create.html'
-    header_icon = 'site'
 
 
-class Edit(EditView):
-    permission_policy = site_permission_policy
-    model = Site
-    form_class = SiteForm
+class EditView(generic.EditView):
     success_message = __("Site '{0}' updated.")
     error_message = __("The site could not be saved due to errors.")
-    delete_item_label = __("Delete site")
-    edit_url_name = 'wagtailsites:edit'
-    index_url_name = 'wagtailsites:index'
-    delete_url_name = 'wagtailsites:delete'
     context_object_name = 'site'
     template_name = 'wagtailsites/edit.html'
-    header_icon = 'site'
 
 
-class Delete(DeleteView):
-    permission_policy = site_permission_policy
-    model = Site
+class DeleteView(generic.DeleteView):
     success_message = __("Site '{0}' deleted.")
-    index_url_name = 'wagtailsites:index'
-    delete_url_name = 'wagtailsites:delete'
     page_title = __("Delete site")
     confirmation_message = __("Are you sure you want to delete this site?")
-    header_icon = 'site'
+
+
+class SiteViewSet(ModelViewSet):
+    icon = 'site'
+    model = Site
+    permission_policy = site_permission_policy
+
+    index_view_class = IndexView
+    add_view_class = CreateView
+    edit_view_class = EditView
+    delete_view_class = DeleteView
+
+    def get_form_class(self, for_update=False):
+        return SiteForm
