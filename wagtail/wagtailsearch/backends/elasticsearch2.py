231dfc72f2bba06da48ba0467932e2e3089aa667
Karl Hobley <karlhobley10@gmail.com>
2016-05-06 11:23:48 +0100

    Split models up into different indices
    pages, images and documents are now in separate indices

--- wagtail/wagtailsearch/backends/elasticsearch2.py
+++ wagtail/wagtailsearch/backends/elasticsearch2.py
@@ -1,7 +1,18 @@
 from .elasticsearch import ElasticsearchSearchBackend
 
 
+def get_model_root(model):
+    if model._meta.parents:
+        return list(model._meta.parents.items())[0][0]
+
+    return model
+
+
 class Elasticsearch2SearchBackend(ElasticsearchSearchBackend):
-    pass
+    def get_index_for_model(self, model):
+        root_model = get_model_root(model)
+        index_suffix = '__' + root_model._meta.app_label.lower() + '_' + root_model.__name__.lower()
+
+        return self.index_class(self, self.index_name + index_suffix)
 
 SearchBackend = Elasticsearch2SearchBackend
