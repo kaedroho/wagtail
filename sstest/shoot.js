24582db73254d4c20940426df1a46fd71a127a62
Karl Hobley <karl@torchbox.com>
2016-08-08 17:27:42 +0100

    Added more pages

--- sstest/shoot.js
+++ sstest/shoot.js
@@ -29,6 +29,14 @@ let tests = [
         ]
     },
     {
+        name: 'pages/password-reset',
+        path: '/admin/password_reset/',
+        user: null,
+        sizes: [
+            [1024, 768],
+        ]
+    },
+    {
         name: 'pages/dashboard',
         path: '/admin/',
         user: 'admin',
@@ -39,11 +47,71 @@ let tests = [
         ]
     },
     {
-        name: 'pages/password-reset',
-        path: '/admin/password_reset/',
-        user: null,
+        name: 'pages/page-explorer-root',
+        path: '/admin/pages/',
+        user: 'admin',
         sizes: [
-            [1024, 768],
+          [1024, 768],
+          [640, 480],
+          [1920, 1200],
+        ]
+    },
+    {
+        name: 'pages/page-explorer',
+        path: '/admin/pages/2/',
+        user: 'admin',
+        sizes: [
+          [1024, 768],
+        ]
+    },
+    {
+        name: 'pages/edit-page',
+        path: '/admin/pages/2/edit/',
+        user: 'admin',
+        sizes: [
+          [1024, 768],
+          [640, 480],
+          [1920, 1200],
+        ]
+    },
+    {
+        name: 'pages/add-page',
+        path: '/admin/pages/2/add_subpage/',
+        user: 'admin',
+        sizes: [
+          [1024, 768],
+          [640, 480],
+          [1920, 1200],
+        ]
+    },
+    {
+        name: 'pages/add-page-editor',
+        path: '/admin/pages/add/demosite/homepage/1/',
+        user: 'admin',
+        sizes: [
+          [1024, 768],
+          [640, 480],
+          [1920, 1200],
+        ]
+    },
+    {
+        name: 'pages/image-index',
+        path: '/admin/images/',
+        user: 'admin',
+        sizes: [
+          [1024, 768],
+          [640, 480],
+          [1920, 1200],
+        ]
+    },
+    {
+        name: 'pages/document-index',
+        path: '/admin/documents/',
+        user: 'admin',
+        sizes: [
+          [1024, 768],
+          [640, 480],
+          [1920, 1200],
         ]
     },
 ];
