9f799a8729dd70f38991afef1d4b1ae4c6085649
Karl Hobley <karl@torchbox.com>
2016-08-25 13:29:02 +0100

    Added convert_where_node_to_query utility function

--- wagtail/wagtailsearch/tests/test_query.py
+++ wagtail/wagtailsearch/tests/test_query.py
@@ -1,8 +1,13 @@
 from __future__ import absolute_import, unicode_literals
 
+import datetime
+
+from django.db.models import Q
 from django.test import TestCase
 
+from wagtail.tests.search import models
 from wagtail.wagtailsearch import query
+from wagtail.wagtailsearch.utils import convert_where_node_to_query
 
 
 class TestCombinators(TestCase):
@@ -32,3 +37,177 @@ class TestCombinators(TestCase):
         self.assertIsInstance(inverted, query.FilterQuery)
         self.assertIsInstance(inverted.query, query.MatchAllQuery)
         self.assertEqual(inverted.exclude, query_a)
+
+
+class TestConvertWhereNodeToQuery(TestCase):
+    def test_simple(self):
+        qs = models.SearchTest.objects.all()
+
+        q = convert_where_node_to_query(qs.query.where)
+
+        self.assertIsNone(q)
+
+    def test_exact_lookup(self):
+        qs = models.SearchTest.objects.filter(title="foo")
+
+        q = convert_where_node_to_query(qs.query.where)
+
+        self.assertIsInstance(q, query.TermQuery)
+        self.assertEqual(q.field, 'title')
+        self.assertEqual(q.value, 'foo')
+
+    def test_startswith_lookup(self):
+        qs = models.SearchTest.objects.filter(title__startswith="foo")
+
+        q = convert_where_node_to_query(qs.query.where)
+
+        self.assertIsInstance(q, query.PrefixQuery)
+        self.assertEqual(q.field, 'title')
+        self.assertEqual(q.prefix, 'foo')
+
+    def test_gt_lookup(self):
+        qs = models.SearchTest.objects.filter(published_date__gt=datetime.date(2016, 8, 28))
+
+        q = convert_where_node_to_query(qs.query.where)
+
+        self.assertIsInstance(q, query.RangeQuery)
+        self.assertEqual(q.from_, datetime.date(2016, 8, 28))
+        self.assertEqual(q.from_included, False)
+        self.assertIsNone(q.to)
+
+    def test_gte_lookup(self):
+        qs = models.SearchTest.objects.filter(published_date__gte=datetime.date(2016, 8, 28))
+
+        q = convert_where_node_to_query(qs.query.where)
+
+        self.assertIsInstance(q, query.RangeQuery)
+        self.assertEqual(q.from_, datetime.date(2016, 8, 28))
+        self.assertEqual(q.from_included, True)
+        self.assertIsNone(q.to)
+
+    def test_lt_lookup(self):
+        qs = models.SearchTest.objects.filter(published_date__lt=datetime.date(2016, 8, 28))
+
+        q = convert_where_node_to_query(qs.query.where)
+
+        self.assertIsInstance(q, query.RangeQuery)
+        self.assertEqual(q.to, datetime.date(2016, 8, 28))
+        self.assertEqual(q.to_included, False)
+        self.assertIsNone(q.from_)
+
+    def test_lte_lookup(self):
+        qs = models.SearchTest.objects.filter(published_date__lte=datetime.date(2016, 8, 28))
+
+        q = convert_where_node_to_query(qs.query.where)
+
+        self.assertIsInstance(q, query.RangeQuery)
+        self.assertEqual(q.to, datetime.date(2016, 8, 28))
+        self.assertEqual(q.to_included, True)
+        self.assertIsNone(q.from_)
+
+    def test_range_lookup(self):
+        qs = models.SearchTest.objects.filter(published_date__range=(datetime.date(2016, 8, 19), datetime.date(2016, 8, 28)))
+
+        q = convert_where_node_to_query(qs.query.where)
+
+        self.assertIsInstance(q, query.RangeQuery)
+        self.assertEqual(q.from_, datetime.date(2016, 8, 19))
+        self.assertEqual(q.from_included, True)
+        self.assertEqual(q.to, datetime.date(2016, 8, 28))
+        self.assertEqual(q.to_included, True)
+
+    def test_in_lookup(self):
+        qs = models.SearchTest.objects.filter(title__in=['foo', 'bar', 'baz'])
+
+        q = convert_where_node_to_query(qs.query.where)
+
+        self.assertIsInstance(q, query.DisjunctionQuery)
+        for subquery in q.subqueries:
+            self.assertIsInstance(subquery, query.TermQuery)
+            self.assertEqual(subquery.field, 'title')
+
+        self.assertEqual(q.subqueries[0].value, 'foo')
+        self.assertEqual(q.subqueries[1].value, 'bar')
+        self.assertEqual(q.subqueries[2].value, 'baz')
+
+    def test_combination_and(self):
+        qs1 = models.SearchTest.objects.filter(title='foo')
+        qs2 = models.SearchTest.objects.filter(title='bar')
+        qs = qs1 & qs2
+
+        q = convert_where_node_to_query(qs.query.where)
+
+        self.assertIsInstance(q, query.ConjunctionQuery)
+        for subquery in q.subqueries:
+            self.assertIsInstance(subquery, query.TermQuery)
+            self.assertEqual(subquery.field, 'title')
+
+        self.assertEqual(q.subqueries[0].value, 'foo')
+        self.assertEqual(q.subqueries[1].value, 'bar')
+
+    def test_combination_or(self):
+        qs1 = models.SearchTest.objects.filter(title='foo')
+        qs2 = models.SearchTest.objects.filter(title='bar')
+        qs = qs1 | qs2
+
+        q = convert_where_node_to_query(qs.query.where)
+
+        self.assertIsInstance(q, query.DisjunctionQuery)
+        for subquery in q.subqueries:
+            self.assertIsInstance(subquery, query.TermQuery)
+            self.assertEqual(subquery.field, 'title')
+
+        self.assertEqual(q.subqueries[0].value, 'foo')
+        self.assertEqual(q.subqueries[1].value, 'bar')
+
+    def test_combination_same_filter(self):
+        qs = models.SearchTest.objects.filter(title='foo', live=True)
+
+        q = convert_where_node_to_query(qs.query.where)
+
+        subqueries_dict = {}
+        self.assertIsInstance(q, query.ConjunctionQuery)
+        for subquery in q.subqueries:
+            self.assertIsInstance(subquery, query.TermQuery)
+            subqueries_dict[subquery.field] = subquery.value
+
+        self.assertEqual(subqueries_dict['title'], 'foo')
+        self.assertEqual(subqueries_dict['live'], True)
+
+    def test_combination_q_object(self):
+        qs = models.SearchTest.objects.filter(Q(title='foo') | Q(live=True))
+
+        q = convert_where_node_to_query(qs.query.where)
+
+        subqueries_dict = {}
+        self.assertIsInstance(q, query.DisjunctionQuery)
+        for subquery in q.subqueries:
+            self.assertIsInstance(subquery, query.TermQuery)
+            subqueries_dict[subquery.field] = subquery.value
+
+        self.assertEqual(subqueries_dict['title'], 'foo')
+        self.assertEqual(subqueries_dict['live'], True)
+
+    def test_negation(self):
+        qs = models.SearchTest.objects.exclude(title="foo")
+
+        q = convert_where_node_to_query(qs.query.where)
+
+        self.assertIsInstance(q, query.FilterQuery)
+        self.assertIsInstance(q.query, query.MatchAllQuery)
+
+        self.assertIsInstance(q.exclude, query.TermQuery)
+        self.assertEqual(q.exclude.field, 'title')
+        self.assertEqual(q.exclude.value, 'foo')
+
+    def test_negation_q_object(self):
+        qs = models.SearchTest.objects.filter(~Q(title='foo'))
+
+        q = convert_where_node_to_query(qs.query.where)
+
+        self.assertIsInstance(q, query.FilterQuery)
+        self.assertIsInstance(q.query, query.MatchAllQuery)
+
+        self.assertIsInstance(q.exclude, query.TermQuery)
+        self.assertEqual(q.exclude.field, 'title')
+        self.assertEqual(q.exclude.value, 'foo')
--- wagtail/wagtailsearch/utils.py
+++ wagtail/wagtailsearch/utils.py
@@ -2,6 +2,15 @@ from __future__ import absolute_import, unicode_literals
 
 import string
 
+from django.db.models.lookups import Lookup
+from django.db.models.query import QuerySet
+from django.db.models.sql.where import SubqueryConstraint, WhereNode
+from django.utils.six import text_type
+
+from wagtail.wagtailsearch.index import class_is_indexed
+
+from . import query
+
 MAX_QUERY_STRING_LENGTH = 255
 
 
@@ -19,3 +28,90 @@ def normalise_query_string(query_string):
     query_string = ' '.join(query_string.split())
 
     return query_string
+
+
+class FieldError(Exception):
+    pass
+
+
+def convert_where_node_to_query(where_node):
+    def _process_filter(field, lookup, value):
+        if lookup == 'exact':
+            return query.TermQuery(field, value)
+
+        if lookup == 'isnull':
+            if value:
+                return query.TermQuery(field, None)
+            else:
+                return ~query.TermQuery(field, None)
+
+        if lookup == 'startswith':
+            return query.PrefixQuery(field, value)
+
+        if lookup == 'gt':
+            return query.RangeQuery(field, from_=value, from_included=False)
+
+        if lookup == 'gte':
+            return query.RangeQuery(field, from_=value, from_included=True)
+
+        if lookup == 'lt':
+            return query.RangeQuery(field, to=value, to_included=False)
+
+        if lookup == 'lte':
+            return query.RangeQuery(field, to=value, to_included=True)
+
+        if lookup == 'range':
+            lower, upper = value
+            return query.RangeQuery(field, from_=lower, from_included=True, to=upper, to_included=True)
+
+        if lookup == 'in':
+            return query.DisjunctionQuery([
+                query.TermQuery(field, term)
+                for term in value
+            ])
+
+        raise FilterError(
+            'Could not apply filter on search results: "' + field + '__' +
+            lookup + ' = ' + text_type(value) + '". Lookup "' + lookup + '"" not recognised.'
+        )
+
+    def _connect_filters(filters, connector, negated):
+        if filters:
+            if len(filters) == 1:
+                filter_out = filters[0]
+            elif connector.lower() == 'or':
+                filter_out = query.DisjunctionQuery([fil for fil in filters if fil is not None])
+            elif connector.lower() == 'and':
+                filter_out = query.ConjunctionQuery([fil for fil in filters if fil is not None])
+
+            if negated:
+                filter_out = ~filter_out
+
+            return filter_out
+
+    # Check if this is a leaf node
+    if isinstance(where_node, Lookup):
+        field_attname = where_node.lhs.target.attname
+        lookup = where_node.lookup_name
+        value = where_node.rhs
+
+        # Ignore pointer fields that show up in specific page type queries
+        if field_attname.endswith('_ptr_id'):
+            return
+
+        # Process the filter
+        return _process_filter(field_attname, lookup, value)
+
+    elif isinstance(where_node, SubqueryConstraint):
+        raise FilterError('Could not apply filter on search results: Subqueries are not allowed.')
+
+    elif isinstance(where_node, WhereNode):
+        # Get child filters
+        connector = where_node.connector
+        child_filters = [convert_where_node_to_query(child) for child in where_node.children]
+        child_filters = [child_filter for child_filter in child_filters if child_filter]
+
+        return _connect_filters(child_filters, connector, where_node.negated)
+
+    else:
+        raise FilterError('Could not apply filter on search results: Unknown where node: ' + str(type(where_node)))
