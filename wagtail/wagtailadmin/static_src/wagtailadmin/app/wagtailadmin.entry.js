58c755b2201683ad59a49bf5da5f70b78f3c8bed
Thibaud Colas <thibaudcolas@gmail.com>
2016-09-22 17:08:07 +0300

    Fix explorer component initialising itself on pages without an explorer

--- wagtail/wagtailadmin/static_src/wagtailadmin/app/wagtailadmin.entry.js
+++ wagtail/wagtailadmin/static_src/wagtailadmin/app/wagtailadmin.entry.js
@@ -11,37 +11,43 @@ import Explorer from 'components/explorer/Explorer';
 import ExplorerToggle from 'components/explorer/toggle';
 import rootReducer from 'components/explorer/reducers';
 
+const initExplorer = () => {
+  const explorerNode = document.querySelector('#explorer');
+  const toggleNode = document.querySelector('[data-explorer-menu-url]');
+
+  if (explorerNode && toggleNode) {
+    const middleware = [
+      // TODO Get rid of redux-logger, solely use the Chrome DevTools extension?
+      createLogger(),
+      thunkMiddleware,
+    ];
+
+    const store = createStore(rootReducer, {}, compose(
+      applyMiddleware(...middleware),
+      // Expose store to Redux DevTools extension.
+      window.devToolsExtension ? window.devToolsExtension() : f => f
+    ));
+
+    const toggle = (
+      <Provider store={store}>
+        <ExplorerToggle label={toggleNode.innerText} />
+      </Provider>
+    );
+
+    const explorer = (
+      <Provider store={store}>
+        <Explorer type="sidebar" defaultPage={1} />
+      </Provider>
+    );
+
+    ReactDOM.render(toggle, toggleNode.parentNode);
+    ReactDOM.render(explorer, explorerNode);
+  }
+};
+
 /**
  * Admin JS entry point. Add in here code to run once the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', () => {
-  const explorerNode = document.querySelector('#explorer');
-  const toggleNode = document.querySelector('[data-explorer-menu-url]');
-
-  const middleware = [
-    // TODO Get rid of redux-logger, solely use the Chrome DevTools extension?
-    createLogger(),
-    thunkMiddleware,
-  ];
-
-  const store = createStore(rootReducer, {}, compose(
-    applyMiddleware(...middleware),
-    // Expose store to Redux DevTools extension.
-    window.devToolsExtension ? window.devToolsExtension() : f => f
-  ));
-
-  const toggle = (
-    <Provider store={store}>
-      <ExplorerToggle label={toggleNode.innerText} />
-    </Provider>
-  );
-
-  const explorer = (
-    <Provider store={store}>
-      <Explorer type="sidebar" defaultPage={1} />
-    </Provider>
-  );
-
-  ReactDOM.render(toggle, toggleNode.parentNode);
-  ReactDOM.render(explorer, explorerNode);
+  initExplorer();
 });
