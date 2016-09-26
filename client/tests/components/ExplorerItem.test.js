f76ad7595e172712d5089b759ef38e7fdad609b3
Thibaud Colas <thibaudcolas@gmail.com>
2016-09-26 17:58:31 +0300

    Remove unused variable

--- client/tests/components/ExplorerItem.test.js
+++ client/tests/components/ExplorerItem.test.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { createStore } from 'redux';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import '../stubs';
 
 import ExplorerItem from '../../src/components/explorer/ExplorerItem';
-import rootReducer from '../../src/components/explorer/reducers';
-
-const store = createStore(rootReducer);
 
 describe('ExplorerItem', () => {
   it('exists', () => {
