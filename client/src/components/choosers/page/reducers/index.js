046dd8d2346b7f9cb2a9f3fd0e2b8560050a20c3
Karl Hobley <karl@torchbox.com>
2016-09-29 15:56:35 +0100

    Fix some terminology inconsistency

--- client/src/components/choosers/page/reducers/index.js
+++ client/src/components/choosers/page/reducers/index.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 
 const defaultState = {
     isFetching: false,
-    pages: [],
+    items: [],
     totalItems: 0,
     pageTypes: {},
     viewName: 'browse',
@@ -18,21 +18,21 @@ export default function pageChooser(state = defaultState, action) {
     switch (action.type) {
         case 'SET_VIEW':
             return _.assign({}, state, {
-                pages: [],
+                items: [],
                 totalItems: 0,
                 viewName: action.payload.viewName,
                 viewOptions: action.payload.viewOptions,
             });
 
-        case 'FETCH_PAGES_START':
+        case 'FETCH_START':
             return _.assign({}, state, {
                 isFetching: true,
             });
 
-        case 'FETCH_PAGES_SUCCESS':
+        case 'FETCH_SUCCESS':
             return _.assign({}, state, {
                 isFetching: false,
-                pages: action.payload.json.items,
+                items: action.payload.json.items,
                 totalItems: action.payload.json.meta.total_count,
                 pageTypes: _.assign({}, state.pageTypes, action.payload.json.__types),
             });
