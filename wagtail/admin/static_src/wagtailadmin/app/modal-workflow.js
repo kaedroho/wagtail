42c148995e68db64e46ff66fbe504de4ef637ddb
Karl Hobley <karl@torchbox.com>
2016-10-03 10:09:00 +0100

    Implemented clear choice button

--- wagtail/admin/static_src/wagtailadmin/app/modal-workflow.js
+++ wagtail/admin/static_src/wagtailadmin/app/modal-workflow.js
@@ -50,42 +50,8 @@ export function createPageChooser(id, modelNames, parent, canChooseRoot) {
         });
     }
 
-/*
-    var chooserElement = $('#' + id + '-chooser');
-    var pageTitle = chooserElement.find('.title');
-    var input = $('#' + id);
-    var editLink = chooserElement.find('.edit-link');
-
-    $('.action-choose', chooserElement).click(function() {
-        var initialUrl = window.chooserUrls.pageChooser;
-        if (openAtParentId) {
-            initialUrl += openAtParentId + '/';
-        }
-
-        var urlParams = {page_type: pageTypes.join(',')};
-        if (canChooseRoot) {
-            urlParams.can_choose_root = 'true';
-        }
-
-        ModalWorkflow({
-            url: initialUrl,
-            urlParams: urlParams,
-            responses: {
-                pageChosen: function(pageData) {
-                    input.val(pageData.id);
-                    openAtParentId = pageData.parentId;
-                    pageTitle.text(pageData.title);
-                    chooserElement.removeClass('blank');
-                    editLink.attr('href', pageData.editUrl);
-                }
-            }
-        });
+    clearButton.addEventListener('click', function() {
+        inputElement.value = '';
+        chooserElement.classList.add('blank');
     });
-
-    $('.action-clear', chooserElement).click(function() {
-        input.val('');
-        openAtParentId = null;
-        chooserElement.addClass('blank');
-    });
-*/
 }
