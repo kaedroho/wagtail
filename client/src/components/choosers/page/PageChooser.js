e26e0ed1aa2e299862c6a0791c2632e85dce0a15
Karl Hobley <karl@torchbox.com>
2016-10-03 15:03:00 +0100

    Change indentation to 2 spaces

--- client/src/components/choosers/page/PageChooser.js
+++ client/src/components/choosers/page/PageChooser.js
@@ -14,73 +14,73 @@ import PageChooserSearchView from './views/PageChooserSearchView';
 
 
 function getTotalPages(totalItems, itemsPerPage) {
-    return Math.ceil(totalItems / itemsPerPage);
+  return Math.ceil(totalItems / itemsPerPage);
 }
 
 class PageChooser extends BaseChooser {
-    renderModalContents() {
-        // Event handlers
-        let onSearch = (queryString) => {
-            if (queryString) {
-                this.props.search(queryString, this.props.restrictPageTypes, 1);
-            } else {
-                // Search box is empty, browse instead
-                this.props.browse('root', 1);
-            }
-        }
-
-        let onNavigate = (page) => {
-            this.props.browse(page.id, 1);
-        };
-
-        let onChangePage = (newPageNumber) => {
-            switch (this.props.viewName) {
-                case 'browse':
-                    this.props.browse(this.props.viewOptions.parentPageID, newPageNumber);
-                    break;
-                case 'search':
-                    this.props.search(this.props.viewOptions.queryString, this.props.restrictPageTypes, newPageNumber);
-                    break;
-            }
-        };
-
-        // Views
-        let view = null;
-        switch (this.props.viewName) {
-            case 'browse':
-                view = <PageChooserBrowseView parentPage={this.props.parent} items={this.props.items} pageTypes={this.props.pageTypes} restrictPageTypes={this.props.restrictPageTypes} pageNumber={this.props.viewOptions.pageNumber} totalPages={getTotalPages(this.props.totalItems, 20)} onPageChosen={this.props.onPageChosen} onNavigate={onNavigate} onChangePage={onChangePage} />;
-                break;
-            case 'search':
-                view = <PageChooserSearchView items={this.props.items} totalItems={this.props.totalItems} pageTypes={this.props.pageTypes} restrictPageTypes={this.props.restrictPageTypes} pageNumber={this.props.viewOptions.pageNumber} totalPages={getTotalPages(this.props.totalItems, 20)} onPageChosen={this.props.onPageChosen} onNavigate={onNavigate} onChangePage={onChangePage} />;
-                break;
-        }
-
-        return (
-            <div>
-                <PageChooserHeader onSearch={onSearch} />
-                {view}
-            </div>
-        );
+  renderModalContents() {
+    // Event handlers
+    let onSearch = (queryString) => {
+      if (queryString) {
+        this.props.search(queryString, this.props.restrictPageTypes, 1);
+      } else {
+        // Search box is empty, browse instead
+        this.props.browse('root', 1);
+      }
     }
 
-    componentDidMount() {
-        this.props.browse(this.props.initialParentPageId || 'root', 1);
+    let onNavigate = (page) => {
+      this.props.browse(page.id, 1);
+    };
+
+    let onChangePage = (newPageNumber) => {
+      switch (this.props.viewName) {
+        case 'browse':
+          this.props.browse(this.props.viewOptions.parentPageID, newPageNumber);
+          break;
+        case 'search':
+          this.props.search(this.props.viewOptions.queryString, this.props.restrictPageTypes, newPageNumber);
+          break;
+      }
+    };
+
+    // Views
+    let view = null;
+    switch (this.props.viewName) {
+      case 'browse':
+        view = <PageChooserBrowseView parentPage={this.props.parent} items={this.props.items} pageTypes={this.props.pageTypes} restrictPageTypes={this.props.restrictPageTypes} pageNumber={this.props.viewOptions.pageNumber} totalPages={getTotalPages(this.props.totalItems, 20)} onPageChosen={this.props.onPageChosen} onNavigate={onNavigate} onChangePage={onChangePage} />;
+        break;
+      case 'search':
+        view = <PageChooserSearchView items={this.props.items} totalItems={this.props.totalItems} pageTypes={this.props.pageTypes} restrictPageTypes={this.props.restrictPageTypes} pageNumber={this.props.viewOptions.pageNumber} totalPages={getTotalPages(this.props.totalItems, 20)} onPageChosen={this.props.onPageChosen} onNavigate={onNavigate} onChangePage={onChangePage} />;
+        break;
     }
+
+    return (
+      <div>
+        <PageChooserHeader onSearch={onSearch} />
+        {view}
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.props.browse(this.props.initialParentPageId || 'root', 1);
+  }
 }
 
 
 const mapStateToProps = (state) => ({
-    viewName: state.viewName,
-    viewOptions: state.viewOptions,
-    parent: state.parent,
-    items: state.items,
-    totalItems: state.totalItems,
-    pageTypes: state.pageTypes,
+  viewName: state.viewName,
+  viewOptions: state.viewOptions,
+  parent: state.parent,
+  items: state.items,
+  totalItems: state.totalItems,
+  pageTypes: state.pageTypes,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    browse: (parentPageID, pageNumber) => dispatch(actions.browse(parentPageID, pageNumber)),
-    search: (queryString, restrictPageTypes, pageNumber) => dispatch(actions.search(queryString, restrictPageTypes, pageNumber)),
+  browse: (parentPageID, pageNumber) => dispatch(actions.browse(parentPageID, pageNumber)),
+  search: (queryString, restrictPageTypes, pageNumber) => dispatch(actions.search(queryString, restrictPageTypes, pageNumber)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(PageChooser);
--- client/src/components/choosers/page/reducers/index.js
+++ client/src/components/choosers/page/reducers/index.js
@@ -2,42 +2,42 @@ import _ from 'lodash';
 
 
 const defaultState = {
-    isFetching: false,
-    parent: null,
-    items: [],
-    totalItems: 0,
-    pageTypes: {},
-    viewName: 'browse',
-    viewOptions: {
-        parentPageID: 'root',
-        pageNumber: 1,
-    }
+  isFetching: false,
+  parent: null,
+  items: [],
+  totalItems: 0,
+  pageTypes: {},
+  viewName: 'browse',
+  viewOptions: {
+    parentPageID: 'root',
+    pageNumber: 1,
+  }
 };
 
 
 export default function pageChooser(state = defaultState, action) {
-    switch (action.type) {
-        case 'SET_VIEW':
-            return _.assign({}, state, {
-                viewName: action.payload.viewName,
-                viewOptions: action.payload.viewOptions,
-            });
+  switch (action.type) {
+    case 'SET_VIEW':
+      return _.assign({}, state, {
+        viewName: action.payload.viewName,
+        viewOptions: action.payload.viewOptions,
+      });
 
-        case 'FETCH_START':
-            return _.assign({}, state, {
-                isFetching: true,
-            });
+    case 'FETCH_START':
+      return _.assign({}, state, {
+        isFetching: true,
+      });
 
-        case 'FETCH_SUCCESS':
-            return _.assign({}, state, {
-                isFetching: false,
-                parent: action.payload.parentJson,
-                items: action.payload.itemsJson.items,
-                totalItems: action.payload.itemsJson.meta.total_count,
-                pageTypes: _.assign({}, state.pageTypes, action.payload.itemsJson.__types),
-            });
+    case 'FETCH_SUCCESS':
+      return _.assign({}, state, {
+        isFetching: false,
+        parent: action.payload.parentJson,
+        items: action.payload.itemsJson.items,
+        totalItems: action.payload.itemsJson.meta.total_count,
+        pageTypes: _.assign({}, state.pageTypes, action.payload.itemsJson.__types),
+      });
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 }
