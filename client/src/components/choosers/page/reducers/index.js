c3e090e60e185ef38e2978fbc8d566128bf722bf
Thibaud Colas <thibaudcolas@gmail.com>
2017-06-13 02:05:21 +0300

    Replace _.assign with Object.assign

--- client/src/components/choosers/page/reducers/index.js
+++ client/src/components/choosers/page/reducers/index.js
@@ -1,6 +1,3 @@
-import _ from 'lodash';
-
-
 const defaultState = {
   isFetching: false,
   error: null,
@@ -15,32 +12,31 @@ const defaultState = {
   }
 };
 
-
 export default function pageChooser(state = defaultState, action) {
   switch (action.type) {
     case 'SET_VIEW':
-      return _.assign({}, state, {
+      return Object.assign({}, state, {
         viewName: action.payload.viewName,
         viewOptions: action.payload.viewOptions,
       });
 
     case 'FETCH_START':
-      return _.assign({}, state, {
+      return Object.assign({}, state, {
         isFetching: true,
         error: null,
       });
 
     case 'FETCH_SUCCESS':
-      return _.assign({}, state, {
+      return Object.assign({}, state, {
         isFetching: false,
         parent: action.payload.parentJson,
         items: action.payload.itemsJson.items,
         totalItems: action.payload.itemsJson.meta.total_count,
-        pageTypes: _.assign({}, state.pageTypes, action.payload.itemsJson.__types),
+        pageTypes: Object.assign({}, state.pageTypes, action.payload.itemsJson.__types),
       });
 
     case 'FETCH_FAILURE':
-      return _.assign({}, state, {
+      return Object.assign({}, state, {
         isFetching: false,
         error: action.payload.message,
         items: [],
