28cea2857c68b5e4edd7a738c8f7ce61bad1122d
Thibaud Colas <thibaudcolas@gmail.com>
2017-06-13 02:05:37 +0300

    Lint page chooser reducer

--- client/src/components/choosers/page/reducers/index.js
+++ client/src/components/choosers/page/reducers/index.js
@@ -9,41 +9,42 @@ const defaultState = {
   viewOptions: {
     parentPageID: 'root',
     pageNumber: 1,
-  }
+  },
 };
 
-export default function pageChooser(state = defaultState, action) {
-  switch (action.type) {
-    case 'SET_VIEW':
-      return Object.assign({}, state, {
-        viewName: action.payload.viewName,
-        viewOptions: action.payload.viewOptions,
-      });
+export default function pageChooser(state = defaultState, { type, payload }) {
+  switch (type) {
+  case 'SET_VIEW':
+    return Object.assign({}, state, {
+      viewName: payload.viewName,
+      viewOptions: payload.viewOptions,
+    });
 
-    case 'FETCH_START':
-      return Object.assign({}, state, {
-        isFetching: true,
-        error: null,
-      });
+  case 'FETCH_START':
+    return Object.assign({}, state, {
+      isFetching: true,
+      error: null,
+    });
 
-    case 'FETCH_SUCCESS':
-      return Object.assign({}, state, {
-        isFetching: false,
-        parent: action.payload.parentJson,
-        items: action.payload.itemsJson.items,
-        totalItems: action.payload.itemsJson.meta.total_count,
-        pageTypes: Object.assign({}, state.pageTypes, action.payload.itemsJson.__types),
-      });
+  case 'FETCH_SUCCESS':
+    return Object.assign({}, state, {
+      isFetching: false,
+      parent: payload.parentJson,
+      items: payload.itemsJson.items,
+      totalItems: payload.itemsJson.meta.total_count,
+      // eslint-disable-next-line no-underscore-dangle
+      pageTypes: Object.assign({}, state.pageTypes, payload.itemsJson.__types),
+    });
 
-    case 'FETCH_FAILURE':
-      return Object.assign({}, state, {
-        isFetching: false,
-        error: action.payload.message,
-        items: [],
-        totalItems: 0,
-      });
+  case 'FETCH_FAILURE':
+    return Object.assign({}, state, {
+      isFetching: false,
+      error: payload.message,
+      items: [],
+      totalItems: 0,
+    });
 
-    default:
-      return state;
+  default:
+    return state;
   }
 }
