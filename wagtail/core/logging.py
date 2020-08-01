ad338d45fe2e446057ddaf48abf87e7e9f1b91e0
Karl Hobley <karl@torchbox.com>
2020-08-01 20:16:48 +0100

    WIP: Make log actions polymorphic
    A wagtail.copy is also a wagtail.create so we no longer have to create
    separate log entries for this.
    
    Gets even more interesting when we add wagtail.copy_for_translation

--- wagtail/core/logging.py
+++ wagtail/core/logging.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from django.utils.translation import gettext_lazy as _
 
 from wagtail.core import hooks
@@ -23,6 +25,12 @@ class LogActionRegistry:
         # Holds the action messsages, keyed by action
         self.messages = {}
 
+        # Holds a mapping of actions to their parents, if the action has specified one.
+        self.parents = {}
+
+        # Like parents, but includes all ancestors for efficient lookup
+        self.ancestors = defaultdict(set)
+
     def scan_for_actions(self):
         if not self.has_scanned_for_actions:
             for fn in hooks.get_hooks(self.hook_name):
@@ -35,10 +43,78 @@ class LogActionRegistry:
     def get_actions(self):
         return self.scan_for_actions()
 
-    def register_action(self, action, label, message):
+    def get_all_variants(self, action):
+        """
+        Returns all variants of an action, following the value passed in to the `parent` parameter on `register_action`.
+
+        For example, say we have the following actions:
+
+            register_action('wagtail.create')
+            # wagtail.copy also counts as a wagtail.create
+            register_action('wagtail.copy', parent='wagtail.create')
+            register_action('wagtail.edit')
+
+        When you call .get_all_variants on 'wagtail.create' you will recieve a list containing both 'wagtail.create' and
+        'wagtail.copy'.
+
+        This is useful for finding all logs that created a page, which should include creations, copies, and imports.
+        """
+        return [
+            variant for variant in self.actions if action in self.ancestors[variant]
+        ]
+
+    def tree(self, action=None):
+        """
+        Returns a full tree of all the actions base, following the value passed in to the `parent` parameter on `register_action`.
+
+        For example, say we have the following actions:
+
+            register_action('wagtail.create')
+            # wagtail.copy also counts as a wagtail.create
+            register_action('wagtail.copy', parent='wagtail.create')
+            register_action('wagtail.edit')
+
+        This returns the following:
+
+            [
+                {
+                    'action': 'wagtail.create',
+                    'children': [
+                        {
+                            'action': 'wagtail.copy',
+                            'children': []
+                        }
+                    ]
+                },
+                {
+                    'action': 'wagtail.edit',
+                    'children': []
+                },
+            ]
+        """
+        if action:
+            return {
+                'action': action,
+                'children': [
+                    self.tree(variant) for variant in self.actions if self.parents.get(variant) == action
+                ]
+            }
+        else:
+            # Return actions without parents
+            return [
+                self.tree(action) for action in self.actions if action not in self.parents
+            ]
+
+    def register_action(self, action, label, message, parent=None):
         self.actions[action] = (label, message)
         self.messages[action] = message
         self.choices.append((action, label))
+
+        if parent is not None:
+            self.parents[action] = parent
+            self.ancestors[action].add(parent)
+            if self.ancestors[parent]:
+                self.ancestors[action].update(self.ancestors[parent])
 
     def get_choices(self):
         self.scan_for_actions()
