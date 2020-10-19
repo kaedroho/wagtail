fb1b5ef97fc0f84999d8e0f9a24399d67932d248
Karl Hobley <karl@kaed.uk>
2020-10-19 18:37:15 +0100

    Add TypeScript typings for Django's gettext utils

--- client/webpack/base.config.js
+++ client/webpack/base.config.js
@@ -72,6 +72,10 @@ module.exports = function exports() {
     // See https://webpack.js.org/configuration/devtool/.
     devtool: 'source-map',
 
+    externals: {
+      'gettext': 'gettext',
+    },
+
     stats: {
       // Add chunk information (setting this to `false` allows for a less verbose output)
       chunks: false,
