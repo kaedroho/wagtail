1f752e91ecf07228a34b592efebbc6cf678ca98b
Karl Hobley <karl@kaed.uk>
2020-10-24 08:46:29 +0100

    Move backend implementation into new app

---
+++ wagtail/shell/templatetags/wagtailshell_tags.py
@@ -0,0 +1,36 @@
+import json
+
+from django import template
+from django.core.serializers.json import DjangoJSONEncoder
+
+from wagtail.admin.menu import admin_menu
+from wagtail.admin.navigation import get_explorable_root_page
+from wagtail.admin.search import admin_search_areas
+from wagtail.admin.staticfiles import versioned_static
+
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def shell_props(context):
+    request = context['request']
+    search_areas = admin_search_areas.search_items_for_request(request)
+    if not search_areas:
+        return ''
+    search_area = search_areas[0]
+
+    explorer_start_page = get_explorable_root_page(request.user)
+
+    return json.dumps({
+        'logoImages': {
+            'mobileLogo': versioned_static('wagtailadmin/images/wagtail-logo.svg'),
+            'desktopLogoBody': versioned_static('wagtailadmin/images/logo-body.svg'),
+            'desktopLogoTail': versioned_static('wagtailadmin/images/logo-tail.svg'),
+            'desktopLogoEyeOpen': versioned_static('wagtailadmin/images/logo-eyeopen.svg'),
+            'desktopLogoEyeClosed': versioned_static('wagtailadmin/images/logo-eyeclosed.svg'),
+        },
+        'searchUrl': search_area.url,
+        'explorerStartPageId': explorer_start_page.id if explorer_start_page else None,
+        'menuItems': admin_menu.as_serializable(request),
+    }, cls=DjangoJSONEncoder)
